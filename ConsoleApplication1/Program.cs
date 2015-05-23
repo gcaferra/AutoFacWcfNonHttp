40ec144e4e685a4a9c099121131918f19e35a119
Giuseppe <g.caferra@outlook.com>
2015-05-23 14:04:08 +0200

    initial import

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Integration.Wcf;
+using WcfService1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Register(c => new ChannelFactory<IService1>(new NetNamedPipeBinding(), new EndpointAddress("net.pipe://wcfservicelocal/service1.svc"))).SingleInstance();
+
+            builder.Register(c => c.Resolve<ChannelFactory<IService1>>().CreateChannel())
+                .As<IService1>()
+                .UseWcfSafeRelease();
+
+            var container = builder.Build();
+            using (var lifetime = container.BeginLifetimeScope())
+            {
+                var service = lifetime.Resolve<IService1>();
+                var result= service.GetData(1);
+
+                Console.WriteLine("Result {0}",result);
+            }
+            
+
+        }
+    }
+
+   
+}
---
+++ WcfService1/App_Code/AppStarter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Integration.Wcf;
+
+namespace WcfService1.App_Code
+{
+    public class AppStarter
+    {
+        public static void AppInitialize()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Service1>().As<IService1>();
+
+            AutofacHostFactory.Container = builder.Build();
+        }
+
+    }
+}
